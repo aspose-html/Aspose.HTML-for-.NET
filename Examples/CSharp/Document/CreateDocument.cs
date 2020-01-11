204ad272375dee58f441c16a93bb3718d4ee59d6
Kashif Iqbal <kashifiqb@gmail.com>
2020-01-11 12:05:57 +0500

    Added new Examples for Creating, Editing and Saving HTML documents using C#

---
+++ Examples/CSharp/Document/CreateDocument.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Aspose.Html.Examples.CSharp.Document
+{
+    class CreateDocument
+    {
+        public static void Run()
+        {
+            CreateSVG();
+            FromScratch();
+            FromLocalFile();
+            FromRemoteURL();
+            FromRemoteURL1();
+            FromHTML();
+            FromStream();
+        }
+
+        private static void CreateSVG()
+        {
+            //ExStart: CreateSVG
+            using (var document = new Aspose.Html.Dom.Svg.SVGDocument("<svg xmlns='http://www.w3.org/2000/svg'><circle cx='50' cy='50' r='40'/></svg>", "about:blank"))
+            {
+                // do some actions over the document here...
+            }
+            //ExEnd: CreateSVG
+        }
+
+        private static void FromScratch()
+        {
+            //ExStart: FromScratch
+            using (var document = new Aspose.Html.HTMLDocument())
+            {
+                // do some actions over the document here...
+            }
+            //ExEnd: FromScratch
+        }
+
+        private static void FromLocalFile()
+        {
+            //ExStart: FromLocalFile
+            string dataDir = RunExamples.GetDataDir_Data();
+            using (var document = new Aspose.Html.HTMLDocument(dataDir+"input.html"))
+            {
+                // do some actions over the document here...
+            }
+            //ExEnd: FromLocalFile
+        }
+
+        private static void FromRemoteURL()
+        {
+            //ExStart: FromRemoteURL
+            using (var document = new Aspose.Html.HTMLDocument("http://your.site.com/"))
+            {
+                // do some actions over the document here...
+            }
+            //ExEnd: FromRemoteURL
+        }
+
+        private static void FromRemoteURL1()
+        {
+            //ExStart: FromRemoteURL1
+            using (var document = new Aspose.Html.HTMLDocument(new Url("http://your.site.com/")))
+            {
+                // do some actions over the document here...
+            }
+            //ExEnd: FromRemoteURL1
+        }
+
+        private static void FromHTML()
+        {
+            //ExStart: FromHTML
+            using (var document = new Aspose.Html.HTMLDocument("<p>my first paragraph</p>", "about:blank"))
+            {
+                // do some actions over the document here...
+            }
+            //ExEnd: FromHTML
+        }
+
+        private static void FromStream()
+        {
+            //ExStart: FromStream
+            using (MemoryStream mem = new MemoryStream())
+            using (StreamWriter sw = new StreamWriter(mem))
+            {
+                sw.Write("<p>my first paragraph</p>");
+
+                // It is important to set the position to the beginning, since HTMLDocument starts the reading exactly from the current position within the stream.
+                sw.Flush();
+                mem.Seek(0, SeekOrigin.Begin);
+
+                using (var document = new Aspose.Html.HTMLDocument(mem, "about:blank"))
+                {
+                    // do some actions over the document here...
+                }
+            }
+            //ExEnd: FromStream
+        }
+    }
+}
---
+++ Examples/CSharp/Document/SaveDocument.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aspose.Html.Examples.CSharp.Document
+{
+    class SaveDocument
+    {
+        public static void Run()
+        {
+
+        }
+        private static void SaveToFile()
+        {
+            //ExStart: SaveToFile
+            string dataDir = RunExamples.GetDataDir_Data();
+
+            using (var document = new Aspose.Html.HTMLDocument("<p>my first paragraph</p>", "about:blank"))
+            {
+                document.Save(dataDir + "SaveToFile_out.html");
+            }
+            //ExEnd: SaveToFile
+        }
+
+        private static void SaveUsingHTMLSaveOptions()
+        {
+            //ExStart: SaveUsingHTMLSaveOptions
+            string dataDir = RunExamples.GetDataDir_Data();
+
+            using (var document = new Aspose.Html.HTMLDocument("<p>my first paragraph</p>", "about:blank"))
+            {
+                // Create options object
+                Aspose.Html.Saving.HTMLSaveOptions options = new Aspose.Html.Saving.HTMLSaveOptions();
+
+                // Set the maximum depth of resource which will be handled.
+                // Depth of 1 means that only resources directly referenced from the saved document will be handled.
+                // Setting this property to -1 will lead to handling of all resources.
+                // Default value is 3
+                options.ResourceHandlingOptions.MaxHandlingDepth = 1;
+
+                // This property is used to set restriction applied to handlers of external resources.
+                // SameHost means that only resources located at the same host will be saved.
+                options.ResourceHandlingOptions.UrlRestriction = Aspose.Html.Saving.UrlRestriction.SameHost;
+
+                // This property is used to setup processing behaviour of any type of resource.
+                // ResourceHandling.Save means all resources will be saved to the output folder
+                options.ResourceHandlingOptions.Default = Aspose.Html.Saving.ResourceHandling.Save;
+
+                // This property is used to set up processing behaviour of particular 'application/javascript' mime type.
+                // In our case all scripts will be skipped during saving
+                options.ResourceHandlingOptions.JavaScript = Aspose.Html.Saving.ResourceHandling.Discard;
+
+                // Save the document
+                document.Save(dataDir + "SaveUsingHTMLSaveOptions_out.html", options);
+            }
+            //ExEnd: SaveUsingHTMLSaveOptions
+        }
+
+        private static void HTMLToMHTML()
+        {
+            //ExStart: HTMLToMHTML
+            string dataDir = RunExamples.GetDataDir_Data();
+
+            using (var document = new Aspose.Html.HTMLDocument(@"
+<link href=""c:\work\style.css"" rel=""stylesheet"">
+<p>my first paragraph</p>", "about:blank"))
+            {
+                // Create corresponding save options
+                var saveOptions = new Aspose.Html.Saving.MHTMLSaveOptions();
+
+                // Set default resource handling behaviour to "embed"
+                saveOptions.ResourceHandlingOptions.Default = Aspose.Html.Saving.ResourceHandling.Embed;
+
+                // Remove URL restrictions because referenced resource is in another domain 
+                saveOptions.ResourceHandlingOptions.UrlRestriction = Aspose.Html.Saving.UrlRestriction.None;
+
+                // Save to .mht file
+                document.Save(dataDir + "HtmlToMhtml_out.mht", saveOptions);
+            }
+            //ExEnd:HTMLToMHTML
+        }
+
+        private static void HTMLToMarkDown()
+        {
+            //ExStart: HTMLToMarkDown
+            string dataDir = RunExamples.GetDataDir_Data();
+            using (var document = new Aspose.Html.HTMLDocument("<h1>heading text</h1>", "about:blank"))
+            {
+                // Create corresponding save options
+                var saveOptions = Aspose.Html.Saving.MarkdownSaveOptions.Git;
+
+                // Save to .md file
+                document.Save(dataDir+ "HTMLToMarkDown_out.md", saveOptions);
+            }
+            //ExEnd: HTMLToMarkDown
+        }
+    }
+}
